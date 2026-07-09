1639dab5fdf417eda163b3876a76ef42e2075b8f
ERICK DE ANDRADE AMADO <erick.amado@aluno.senac.rio>
2026-07-08 21:34:47 -0300

    Primeiro envio de arquivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EpiList } from './epi-list/epi-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EpiList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('seguranca-frontend');
+  // protected readonly title = signal('Primeira aula de angular');
+  titulo = "Sistema de controle de Epis";
+  subtitulo = "Gestão des segurança do trabalho";
+  descricao = "Aplicação para controle, monitoramento e conformidade dos equipamentos de proteção individual";
+  // professor = "Elias Santos";
 }
