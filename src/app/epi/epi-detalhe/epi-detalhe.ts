a2f5fe333b380df4896e6e8f6af8a2dc6f543290
ERICK DE ANDRADE AMADO <erick.amado@aluno.senac.rio>
2026-07-10 18:51:28 -0300

    criei uma componente e movi a o epi-list para a pasta epi

---
+++ src/app/epi/epi-detalhe/epi-detalhe.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epi-detalhe',
+  imports: [],
+  templateUrl: './epi-detalhe.html',
+  styleUrl: './epi-detalhe.scss',
+})
+export class EpiDetalhe {}
